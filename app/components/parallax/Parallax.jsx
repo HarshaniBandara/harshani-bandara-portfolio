8f28861c8df39a760209bfc9046cc5e785d6eb65
HarshaniBandara <harshani.19@cse.mrt.ac.lk>
2023-12-30 15:21:27 +0530

    Update Parallax.jsx

--- app/components/parallax/Parallax.jsx
+++ app/components/parallax/Parallax.jsx
@@ -28,27 +28,61 @@ const Parallax = ({ type }) => {
         <motion.h3 style={{ y: yText }}>
           {type === "services" ? "Experience" : "Education"}
         </motion.h3>
-
-        <p>Dialog axiata PLC</p>
-        <ul>
-          <li>Tranee Software Engineer</li>
-          <li>from 2023.01.02 - 2023.06.30 (6 months)</li>
-          <li></li>
-        </ul>
-
-        <br />
-        <p>University of Moratuwa</p>
-        <ul>
-          <li>visiting Teaching Assistent</li>
-          <li>from 2023.01.02 - 2023.06.30 (6 months)</li>
-          <li>
-            Modules :
+        {type === "services" && (
+          <div>
+            {" "}
+            <p>Dialog axiata PLC</p>
             <ul>
-              <li>Data Base Management System</li>
-              <li>Programming Fundermentals</li>
+              <li>&nbsp; &nbsp; &nbsp;&nbsp;Trainee Software Engineer</li>
+              <li>&nbsp; &nbsp; &nbsp;&nbsp;from 2023.01.02 - 2023.06.30 (6 months)</li>
+              <li></li>
             </ul>
-          </li>
-        </ul>
+            <br />
+            <p>University of Moratuwa</p>
+            <ul>
+              <li>&nbsp; &nbsp; &nbsp;&nbsp;visiting Teaching Assistent</li>
+              <li>&nbsp; &nbsp; &nbsp;&nbsp;from 2023.01.02 - 2023.06.30 (6 months)</li>
+              <li>
+              &nbsp; &nbsp; &nbsp;&nbsp;Modules :
+                <ul>
+                  <li>&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;Data Base Management System</li>
+                  <li>&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;Programming Fundermentals</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        )}
+        {type !== "services" && (
+          <div>
+            {" "}
+            <p>University of Moratuwa</p>
+            <ul>
+              <li>
+                {" "}
+                B.Sc. Engineering (Hons) in Computer Science & Engineering
+                undergraduate
+              </li>
+              <li>&nbsp; &nbsp; &nbsp;&nbsp;GPA : 3.738</li>
+              <li>&nbsp; &nbsp; &nbsp;&nbsp;Dean's List: Semester 1 | Semester 2 | Semester 6</li>
+              <li>&nbsp; &nbsp; &nbsp;&nbsp;2020-present</li>
+            </ul>
+            <br />
+            <p>St' Josephs Balika Maha vidyalaya- Kegalle</p>
+            <ul>
+             <li>&nbsp; &nbsp; &nbsp;&nbsp;GCE A/L: 3As in physical science stream ( 2018 August)</li>
+             
+              <li>&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;z-score: 2.2452 </li>
+              <li>&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;District rank: 1</li>
+              <br />
+              <li>
+              &nbsp; &nbsp; &nbsp;&nbsp;GCE O/L: 9 As ( 2015 December)
+                <ul>
+                  <li>&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;2010-2018</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
 
       {/* <motion.div className="mountains"></motion.div> */}
